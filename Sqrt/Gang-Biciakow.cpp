8d2cb24102dbb51953e4cf08bd12dd8b4da92ee4
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-11-02 22:36:25 +0100

    Update Gang-Biciakow.cpp

--- Sqrt/Gang-Biciakow.cpp
+++ Sqrt/Gang-Biciakow.cpp
@@ -1,121 +1,113 @@
-//XXVIII OI
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
 
-#define ff first
-#define ss second
-
-const int MAXN = 1e5 + 3, MAXM = 1.5e5 + 3, SQRT = 333;
+const int MAXN = 1e5 + 3, SQRT = 317;
 
 vector <pair <int, int>> adj[MAXN];
 pair <int, int> edges[MAXN];
-vector <int> big[SQRT + 2];
-int biciak[MAXN], big_id[MAXN], normal_id[MAXN], parent[MAXN], edge_to_parent[MAXN], ile[SQRT + 2], in[MAXN], out[MAXN], timer, cnt_big;
+vector <int> freq[SQRT + 1];
+int typ[MAXN], big_id[MAXN], normal_id[MAXN], parent[MAXN], edge_to_par[MAXN], cnt[SQRT + 1], pre[MAXN], post[MAXN], timer, cnt_big;
 
-//big[v] - wektor częstości biciaków na sciezce do korzenia z wierzcholka o duzym indeksie v 
-
-int DFS_precomp(int v, int par, int depth, vector <int> &freq, int rozne, int edge_id){
-    in[v] = timer++;
-    parent[v] = par;
-    edge_to_parent[v] = edge_id;
-    bool flag = false;
-    int max_path = 0;
-    for (auto child : adj[v]){
-        if (child.ff != par){
-            freq[biciak[child.ss]]++;
-            if (freq[biciak[child.ss]] == 1){
-                flag = true;
-            }
-            max_path = max(max_path, DFS_precomp(child.ff, v, depth + 1, freq, rozne + flag, child.ss) + 1);
-            freq[biciak[child.ss]]--;
-        }
-    }
-    out[v] = timer++;
-    if (depth % SQRT == 0 && max_path >= SQRT){
-        big_id[v] = ++cnt_big;
-        normal_id[cnt_big] = v;
-        big[big_id[v]] = freq;
-        ile[big_id[v]] = rozne;
-    }
-    return max_path;
+int DFS(int v, int par, int dep, int distinct, int e_id, vector <int> &f){
+	pre[v] = timer++;
+	parent[v] = par;
+	edge_to_par[v] = e_id;
+	bool nowy = false;
+	int deepest_path = 0;
+	
+	for (auto [u, id] : adj[v]){
+		if (u != par){
+			f[typ[id]]++;
+			if (f[typ[id]] == 1){
+				nowy = true;
+			}
+			deepest_path = max(deepest_path, DFS(u, v, dep + 1, distinct + nowy, id, f) + 1);
+			f[typ[id]]--;
+		}
+	}
+	
+	if (dep % SQRT == 0 && deepest_path >= SQRT){
+		big_id[v] = ++cnt_big;
+		normal_id[big_id[v]] = v;
+		freq[big_id[v]] = f;
+		cnt[big_id[v]] = distinct;
+	}
+	
+	post[v] = timer++;
+	return deepest_path;
 }
 
 int query(int v){
-    unordered_set <int> seen;
-    while (parent[v] != -1 && big_id[v] == 0){
-        seen.insert(biciak[edge_to_parent[v]]);
-        v = parent[v];
-    }
-    if (parent[v] == -1){
-        return seen.size();
-    }
-    int ans = 0;
-    for (int b : seen){
-        if (big[big_id[v]][b] == 0){
-            ans++;
-        }
-    }
-    return ans + ile[big_id[v]];
+	set <int> seen;
+	while (parent[v] != -1 && big_id[v] == 0){
+		seen.insert(typ[edge_to_par[v]]);
+		v = parent[v];
+	}
+	
+	if (parent[v] == -1){
+		return seen.size();
+	}
+	
+	int ans = cnt[big_id[v]];
+	for (int b : seen){
+		if (freq[big_id[v]][b] == 0){
+			ans++;
+		}
+	}
+	return ans;
 }
 
 void update(int id, int nowy){
-    int so_far = biciak[id], u = edges[id].ff, v = edges[id].ss, w;
-    biciak[id] = nowy;
-    if (parent[v] == u){ 
-        swap(u, v);
-    }
-    for (int i = 1; i <= cnt_big; i++){
-        w = normal_id[i];
-        if (in[w] >= in[u] && out[w] <= out[u]){
-            big[i][so_far]--;
-            if (big[i][so_far] == 0){
-                ile[i]--;
-            }
-            big[i][nowy]++;
-            if (big[i][nowy] == 1){
-                ile[i]++;
-            }
-        }
-    }
-}
-
-void solve(){
-	int n, m, q, a, b, c, v, id, x;
-    char ch;
-    cin >> n >> m >> q;
-    for (int i = 1; i <= n - 1; i++){
-        cin >> a >> b >> c;
-        adj[a].emplace_back(b, i);
-        adj[b].emplace_back(a, i);
-        biciak[i] = c;
-        edges[i] = {a, b};
-    }
-    vector <int> freq(m + 1);
-    DFS_precomp(1, -1, 0, freq, 0, -1);
-    while (q--){
-        cin >> ch;
-        if (ch == 'Z'){
-            //ile rodzajow na sciezce do v
-            cin >> v;
-            cout << query(v) << "\n";
-        }
-        else{
-            //na i-tej drodze teraz jest biciak x
-            cin >> id >> x;
-            update(id, x);
-        }
-    }
+	int stary = typ[id];
+	typ[id] = nowy;
+	auto[u, v] = edges[id];
+	if (parent[u] == v){
+		swap(u, v); // v głębiej
+	}
+	for (int i = 1; i <= cnt_big; i++){
+		int big = normal_id[i];
+		if (pre[big] >= pre[v] && post[big] <= post[v]){
+			freq[i][stary]--;
+			if (freq[i][stary] == 0){
+				cnt[i]--;
+			}
+			freq[i][nowy]++;
+			if (freq[i][nowy] == 1){
+				cnt[i]++;
+			}
+		}
+	}
 }
 
 int main(){
-	ios_base::sync_with_stdio(0);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int t;
-	t = 1;
-	//cin >> t;
-	while (t--){
-		solve();
+
+	int n, m, q, a, b, t, v, id, x;
+	char c;
+	cin >> n >> m >> q;
+	for (int i = 1; i < n; i++){
+		cin >> a >> b >> t;
+		adj[a].emplace_back(b, i);
+		adj[b].emplace_back(a, i);
+		typ[i] = t;
+		edges[i] = {a, b};
 	}
+	vector <int> f(m + 1);
+	DFS(1, -1, 0, 0, -1, f);
+
+	while (q--){
+		cin >> c;
+		if (c == 'Z'){
+			cin >> v;
+			cout << query(v) << "\n";
+		}
+		else{
+			cin >> id >> x;
+			update(id, x);
+		}
+	}
+
 	return 0;
 }
