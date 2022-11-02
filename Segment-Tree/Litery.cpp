02641ad50ae3b7b5b42d19159e4957152521abd8
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-11-02 22:33:28 +0100

    Update Litery.cpp

--- Segment-Tree/Litery.cpp
+++ Segment-Tree/Litery.cpp
@@ -1,70 +1,61 @@
-//XIX OI
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
 
-const int maxn = 1e6 + 5, pow2 = (1 << 20);
+const int BASE = (1 << 20);
 
-int tree[2 * pow2]; 
+lli st[2 * BASE + 3];
 
-void update(int i, int val){
-    int id = pow2 + i;
-    tree[id] = val;
-    while (id > 1){
-        id /= 2;
-        tree[id] = tree[2 * id] + tree[2 * id + 1];
-    }
+lli query(int l, int r){
+	l += BASE;
+	r += BASE;
+	lli ans = st[l];
+	if (l != r){
+		ans += st[r];
+	}
+	while (l / 2 != r / 2){
+		if (l % 2 == 0){
+			ans += st[l + 1];
+		}
+		if (r % 2 == 1){
+			ans += st[r - 1];
+		}
+		l /= 2;
+		r /= 2;
+	}
+	return ans;
 }
 
-int query(int l, int r){
-    int id_l = pow2 + l, id_r = pow2 + r;
-    int res = tree[id_l];
-    if (id_l != id_r){
-        res += tree[id_r];
-    }
-    while (id_l / 2 != id_r / 2){
-        if (id_l % 2 == 0){
-            res += tree[id_l + 1];
-        }
-        id_l /= 2;
-        if (id_r % 2 == 1){
-            res += tree[id_r - 1];
-        }
-        id_r /= 2;
-    }
-    return res;
-}
-
-void solve(){
-    int n, pos;
-    lli ans = 0;
-    string s, t;
-    cin >> n;
-    cin >> s >> t;
-    vector <stack <int>> next_occ(27);
-    for (int i = n - 1; i >= 0; i--){
-        next_occ[s[i] - 'A'].push(i);
-    }
-    int c;
-    for (int i = 0; i < n; i++){
-        c = t[i] - 'A';
-        pos = next_occ[c].top();
-        next_occ[c].pop();
-        update(pos, 1);
-        pos += query(pos + 1, n);
-        ans += pos - i; 
-    }
-    cout << ans << "\n";
+void update(int i){
+	i += BASE;
+	st[i]++;
+	while (i > 1){
+		i /= 2;
+		st[i] = st[2 * i] + st[2 * i + 1];
+	}
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int tc;
-    //cin >> tc;
-    tc = 1;
-    while (tc--){
-        solve();
-    }
+	
+	int n;
+	cin >> n;
+	string s, t;
+	cin >> s >> t;
+	vector <queue <int>> positions(26);
+	for (int i = 0; i < n; i++){
+		positions[s[i] - 'A'].push(i);
+	}
+	
+	lli ans = 0;
+	for (int i = 0; i < n; i++){
+		int closest = positions[t[i] - 'A'].front();
+		positions[t[i] - 'A'].pop();
+		ans += closest - i + query(closest + 1, n); // roznica pozycji + o ile przesunęła się litera
+		update(closest);
+	}
+	cout << ans << "\n";
+	
     return 0;
 }
