a51e2032cbe7b6780027512065fe33205731130c
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-11-02 22:36:03 +0100

    Update Kontenery.cpp

--- Sqrt/Kontenery.cpp
+++ Sqrt/Kontenery.cpp
@@ -3,65 +3,48 @@
 typedef long long int lli;
 using namespace std;
 
-const int SQRT = 347;
-
-struct Q{
-    int a, l, d;
-    bool operator <(Q q){
-        return d < q.d;
-    }
-};
-
-void solve(){
-	int n, q, a, l, d;
-    cin >> n >> q;
-    vector <int> ans(n), add(n + 1), stop(n + 1);
-    vector <Q> queries;
-    for (int i = 0; i < q; i++){
-        cin >> a >> l >> d;
-        a--;
-        if (l <= SQRT){
-            for (int i = 0; i < l; i++){
-                ans[a + i * d]++;
-            }
-        }
-        else{
-            queries.push_back({a, l, d});
-        }
-    }
-    sort(queries.begin(), queries.end());
-    Q cur;
-    q = queries.size();
-    for (int i = 0; i < q; i++){
-        cur = queries[i];
-        add[cur.a]++;
-        stop[cur.a + (cur.l - 1) * cur.d]++;
-        if (i == q - 1 || queries[i + 1].d != cur.d){
-            for (int j = 0; j < n; j++){
-                ans[j] += add[j];
-                add[j] -= stop[j];
-                if (add[j] && j + cur.d <= n - 1){
-                    add[j + cur.d] += add[j];
-                }
-                add[j] = 0;
-                stop[j] = 0;
-            }
-        }
-    }
-    for (int x : ans){
-        cout << x << " ";
-    }
-    cout << "\n";
-}
+const int SQRT = 317;
 
 int main(){
-	ios_base::sync_with_stdio(0);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int t;
-	t = 1;
-	//cin >> t;
-	while (t--){
-		solve();
+
+	int n, q, start, steps, d;
+	cin >> n >> q;
+	vector <int> ans(n), add(n), sub(n);
+	vector <vector <pair <int, int>>> big_queries(n, vector <pair <int, int>>());
+	while (q--){
+		cin >> start >> steps >> d;
+		start--;
+		if (d >= SQRT){
+			for (int i = 0; i < steps; i++){
+				ans[start + i * d]++;
+			}
+		}
+		else{
+			big_queries[d].emplace_back(start, steps);
+		}
 	}
+	for (int d = 1; d < min(n, SQRT); d++){
+		for (auto [start, steps] : big_queries[d]){
+			add[start]++;
+			sub[start + (steps - 1) * d]++;
+		}
+		for (int i = 0; i < n; i++){
+			ans[i] += add[i];
+			add[i] -= sub[i];
+			if (i + d < n){
+				add[i + d] += add[i];
+			}
+			add[i] = 0;
+			sub[i] = 0;
+		}
+	}
+
+	for (int x : ans){
+		cout << x << " ";
+	}
+	cout << "\n";
+
 	return 0;
 }
