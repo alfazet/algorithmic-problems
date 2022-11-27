629dbfce3775fd623d5ab4b6e6e63d261610dc61
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-11-27 09:47:24 +0100

    Update and rename kod.cpp to 02/kod.cpp

---
+++ 02/kod.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+typedef long long int lli;
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree <int, null_type, greater <int>, rb_tree_tag, tree_order_statistics_node_update> oset;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector <int> a(n);
+	oset st;
+	for (int i = 0; i < n; i++){
+		cin >> a[i];
+		st.insert(i + 1);
+	}
+	vector <int> ans(n);
+	for (int i = n - 1; i >= 0; i--){
+		if (a[i] >= (int) st.size()){
+			cout << "NIE\n";
+			return 0;
+		}
+		auto it = st.find_by_order(a[i]);
+		ans[i] = *it;
+		st.erase(it);
+	}
+	for (int x : ans){
+		cout << x << "\n";
+	}
+
+	return 0;
+}
