731ec03448f20aa1951d8d18a51ed05dbc3be18f
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-11-29 21:20:45 +0100

    Update aut.cpp

--- aut.cpp
+++ aut.cpp
@@ -1,55 +1,52 @@
-//XII OI
 #include <bits/stdc++.h>
 typedef long long int lli;
-using namespace std;	
+using namespace std;
+
+#define ff first
+#define ss second
 
 struct Stop{
-	int x, y, n;
+	int x, y, p;
 	
-	void print(){
-		cout << "(" << x << ", " << y << ", " << n << ")\n";
+	bool operator < (const Stop &s) const{
+		if (x == s.x){
+			return y < s.y;
+		}
+		return x < s.x;
+	}
+	
+	void read(){
+		cin >> x >> y >> p;
 	}
 };
 
-bool comp(Stop s1, Stop s2){
-	if (s1.x == s2.x){
-		return s1.y < s2.y;
-	}
-	return s1.x < s2.x;
-}
-
-void solve(){
-	int W, H, k;
-	cin >> W >> H >> k;
-	vector <Stop> stops(k);
-	for (int i = 0; i < k; i++){
-		cin >> stops[i].x >> stops[i].y >> stops[i].n;
-	}
-	sort(stops.begin(), stops.end(), comp);
-	map <int, int> mp;
-	mp[0] = 0;
-	int here;
-	for (int i = 0; i < k; i++){
-		auto it = mp.upper_bound(stops[i].y);
-		it--;
-		here = it->second + stops[i].n;
-		it++;
-		while (it != mp.end() && it->second < here){
-			mp.erase(it++);
-		}
-		mp[stops[i].y] = max(mp[stops[i].y], here);	
-
-	}
-	cout << (mp.rbegin())->second << "\n";
-}
-
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t = 1;
-    // cin >> t;
-    while (t--){
-        solve();
-    }
-    return 0;
-}   
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n, m, k;
+	cin >> n >> m >> k;
+	vector <Stop> a(k);
+	for (int i = 0; i < k; i++){
+		a[i].read();
+	}
+	sort(a.begin(), a.end()); // sorted by x, then y
+	
+	lli here;
+	map <int, lli> max_at; // max_at[y] = max. number of people we can collect up to y
+	max_at[0] = 0;
+	for (int i = 0; i < k; i++){
+		auto[x, y, p] = a[i];
+		auto it = max_at.upper_bound(y);
+		it--; // first non-greater
+		here = p + it->ss;
+		it++;
+		while (it != max_at.end() && it->ss < here){ // removing every pair where we are higher, but with less people
+			max_at.erase(it++);
+		}
+		max_at[y] = max(max_at[y], here);
+	}
+	cout << max_at.rbegin()->ss << "\n";
+
+	return 0;
+}
