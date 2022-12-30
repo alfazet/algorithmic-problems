bceff83a027ad5df120b97f3e0246de037524dd5
Antoni Zasada <85628860+H4ntek@users.noreply.github.com>
2022-12-30 13:46:48 +0100

    Update and rename grz.cpp to 14/grz.cpp

--- 14/grz.cpp
+++ 14/grz.cpp
@@ -31,44 +31,44 @@ bool DFS(int r, int c, int typ){
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	
 	cin >> n;
 	for (int r = 0; r < n; r++){
-		for (int c = 0; c < n; c++){
-			cin >> a[r][c];
-		}
+	    for (int c = 0; c < n; c++){
+	        cin >> a[r][c];
+	    }
 	}
-
+	
 	for (int r = 0; r < n; r++){
-		for (int c = 0; c < n; c++){
-			valley[r][c] = true;
-			ridge[r][c] = true;
-			for (auto [dr, dc] : moves){
-				if (valid(r + dr, c + dc)){
-					if (a[r][c] > a[r + dr][c + dc]){
-						valley[r][c] = false;
-					}
-					else if (a[r][c] < a[r + dr][c + dc]){
-						ridge[r][c] = false;
-					}
-				}
-			}
-		}
+	    for (int c = 0; c < n; c++){
+	        valley[r][c] = true;
+	        ridge[r][c] = true;
+	        for (auto [dr, dc] : moves){
+	            if (valid(r + dr, c + dc)){
+	                if (a[r][c] > a[r + dr][c + dc]){
+	                    valley[r][c] = false;
+	                }
+	                else if (a[r][c] < a[r + dr][c + dc]){
+	                    ridge[r][c] = false;
+	                }
+	            }
+	        }
+	    }
 	}
-
+	
 	for (int r = 0; r < n; r++){
-		for (int c = 0; c < n; c++){
-			if (!vis[r][c]){
-				if (valley[r][c]){
-					valleys += DFS(r, c, 0);
-				}
-				if (ridge[r][c]){
-					ridges += DFS(r, c, 1);
-				}
-			}
-		}
+	    for (int c = 0; c < n; c++){
+	        if (!vis[r][c]){
+	            if (valley[r][c]){
+	                valleys += DFS(r, c, 0);
+	            }
+	            if (ridge[r][c]){
+	                ridges += DFS(r, c, 1);
+	            }
+	        }
+	    }
 	}
 	cout << ridges << " " << valleys << "\n";
-
+	
 	return 0;
 }
