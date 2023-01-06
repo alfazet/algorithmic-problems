61c407a00c203774313ed2958c93d9cbe3eda610
Antoni Zasada <85628860+H4ntek@users.noreply.github.com>
2023-01-06 22:08:37 +0100

    Update and rename sta.cpp to 15/sta.cpp

--- 15/sta.cpp
+++ 15/sta.cpp
@@ -1,37 +1,37 @@
-//XV OI
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
 
-const int maxn = 1e6 + 3;
-int n;
-vector <int> adj[maxn];
-int sz[maxn];
-lli sum_dist[maxn], sum_dist_all[maxn];
+const int MAXN = 1e6 + 3;
+
+vector <int> adj[MAXN];
+int sub_sz[MAXN], n; 
+lli sum_dist_sub[MAXN], sum_dist_all[MAXN]; // sums of distances: to all nodes in the subtree / all other nodes
 
 void DFS(int v, int par){
-    sz[v] = 1;
+    sub_sz[v] = 1;
     for (int child : adj[v]){
         if (child != par){
             DFS(child, v);
-            sz[v] += sz[child];
-            sum_dist[v] += sum_dist[child] + sz[child];
+            sum_dist_sub[v] += sum_dist_sub[child] + sub_sz[child]; 
+            sub_sz[v] += sub_sz[child];
         }
     }
 }
 
 void DFS_reroot(int v, int par){
-    if (par != -1){
-        sum_dist_all[v] = sum_dist_all[par] + n - 2 * sz[v];
-    }
     for (int child : adj[v]){
         if (child != par){
+            sum_dist_all[child] = sum_dist_all[v] - sub_sz[child] + (n - sub_sz[child]);
             DFS_reroot(child, v);
         }
     }
 }
 
-void solve(){
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int a, b;
     cin >> n;
     for (int i = 0; i < n - 1; i++){
@@ -39,9 +39,11 @@ void solve(){
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
+
     DFS(1, -1);
-    sum_dist_all[1] = sum_dist[1];
+    sum_dist_all[1] = sum_dist_sub[1];
     DFS_reroot(1, -1);
+
     int ans = 1;
     for (int i = 1; i <= n; i++){
         if (sum_dist_all[i] > sum_dist_all[ans]){
@@ -49,16 +51,6 @@ void solve(){
         }
     }
     cout << ans << "\n";
-}
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    t = 1;
-    //cin >> t;
-    while (t--){
-        solve();
-    }
     return 0;
 }
