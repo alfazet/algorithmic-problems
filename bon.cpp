8aca2a75cd3e984f87fbef995b2e52adc835fe4a
Antoni Zasada <85628860+H4ntek@users.noreply.github.com>
2023-01-12 16:24:03 +0100

    Update bon.cpp

--- bon.cpp
+++ bon.cpp
@@ -1,58 +1,52 @@
-//XIX OI
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
 
 const int MAXN = 1e6 + 3;
 
-int mx;
-lli cust_no = 1;
-bool bon[MAXN], bought[MAXN];
-vector <lli> res;
-int last_bought[MAXN];
-
-void query(int q){
-    lli done = 0;
-    for (int i = last_bought[q] + q; i <= mx && done < q; i += q){
-        if (!bought[i]){
-            bought[i] = true;
-            last_bought[q] = i;
-            if (bon[i]){
-                res.push_back(cust_no + done);
-            }
-            done++;
-        }
-    }
-}
-
-void solve(){
-    int n, x, q;
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        cin >> x;
-        mx = max(mx, x);
-        bon[x] = true;
-    }
-    cin >> q;
-    for (int i = 0; i < q; i++){
-        cin >> x;
-        query(x);
-        cust_no += x;
-    }
-    cout << res.size() << "\n";
-    for (lli k : res){
-        cout << k << "\n";
-    }
-}
+bool good[MAXN], taken[MAXN];
+int next_to_take[MAXN]; // next_to_take[i] = next available multiple of i
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    t = 1;
-    //cin >> t;
-    while (t--){
-        solve();
+	
+    int m, x;
+    cin >> m;
+    for (int i = 0; i < m; i++){
+        cin >> x;
+        good[x] = true;
     }
+    iota(next_to_take, next_to_take + MAXN, 0);
+
+    int n;
+    lli client = 0;
+    vector <lli> ans;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> x;
+        for (int j = 0; j < x; j++){
+            client++;
+            while (next_to_take[x] < MAXN && taken[next_to_take[x]]){
+                next_to_take[x] += x;
+            }
+            if (next_to_take[x] >= MAXN){
+                client += x - j - 1;
+                break;
+            }
+
+            int we_take = next_to_take[x];
+            taken[we_take] = true;
+            if (good[we_take]){
+                ans.push_back(client);
+            }
+        }
+    }
+
+    cout << ans.size() << "\n";
+    for (lli c : ans){
+        cout << c << "\n";
+    }
+
     return 0;
-}
+}   
