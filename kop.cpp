cc3f53753a806ade17f283d359af5def556a93b8
Antoni Zasada <85628860+H4ntek@users.noreply.github.com>
2023-01-15 12:52:18 +0100

    Update kop.cpp

--- kop.cpp
+++ kop.cpp
@@ -1,87 +1,135 @@
-//VIII OI
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
 
-struct P{
-    int x, y, typ;
+const int DELTA = 3e4 + 3;
 
-    bool operator < (P p1){
-        if (x == p1.x){
-            if (typ == p1.typ){
-                return y < p1.y;
+struct P{
+    int x, y, type;
+
+    P(int x_ = 0, int y_ = 0, int type_ = 0) : x(x_), y(y_), type(type_) {};
+
+    bool operator < (P &b){
+        if (x == b.x){
+            if (y == b.y){
+                return type > b.type;
             }
-            return typ < p1.typ;
+            return y < b.y;
         }
-        return x < p1.x;
+        return x < b.x;
     }
 };
 
-const int MAXN = 15003, ST_SZ = (1 << 18), MAX_COORD = 70003;
+struct Max{
+    static const int neutral = 0;
+    int val;
 
-P points[2 * MAXN];
-int st[ST_SZ], lazy[ST_SZ];
+    Max(int val_ = 0) : val(val_) {};
 
-void propagate(int si, int left, int right){
-    st[left] += lazy[si];
-    st[right] += lazy[si];
-    lazy[left] += lazy[si];
-    lazy[right] += lazy[si];
-    lazy[si] = 0;
-}
-
-void update(int si, int ss, int se, int us, int ue, int val){
-    if (se < us || ss > ue){
-        return;
+    Max op(Max other){
+        return Max(max(val, other.val));
     }
-    if (ss >= us && se <= ue){
-        st[si] += val;
-        lazy[si] += val;
-        return;
+    Max lazy_op(Max v){
+        return Max(val + v.val);
+    }
+};
+
+template <typename T>
+struct SegTree{
+    int n;
+    vector <T> st, lazy;
+
+    void init(int n_){
+        n = n_;
+        int base = 1;
+        while (base <= n){
+            base *= 2;
+        }
+        st.resize(2 * base + 3);
+        lazy.resize(2 * base + 3);
     }
 
-    int left = 2 * si, right = 2 * si + 1, mid = (ss + se) / 2;
-    propagate(si, left, right);
-    update(left, ss, mid, us, ue, val);
-    update(right, mid + 1, se, us, ue, val);
-    st[si] = max(st[left], st[right]);
-}
-
-int query(){
-    return st[1];
-}
-
-void solve(){
-    int s, w, n;
-    cin >> s >> w;
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        cin >> points[i].x >> points[i].y;
-        points[i].x += 30000;
-        points[i].y += 30000;
-        points[i].typ = 1;
-
-        points[i + n].x = points[i].x + s + 1;
-        points[i + n].y = points[i].y;
-        points[i + n].typ = -1;
+    void propagate(int i, int s, int e, T val){
+        st[i] = st[i].lazy_op(val);
+        if (s != e){
+            lazy[2 * i].val += val.val;
+            lazy[2 * i + 1].val += val.val;
+        }
     }
-    sort(points, points + 2 * n);
-    int ans = 0;
-    for (int i = 0; i < 2 * n; i++){
-        update(1, 1, MAX_COORD, points[i].y, points[i].y + w, points[i].typ);
-        ans = max(ans, query());
+
+    T query(int i, int s, int e, int l, int r){
+        if (lazy[i].val != 0){
+            propagate(i, s, e, lazy[i]);
+            lazy[i].val = 0;
+        }
+
+        if (r < s || e < l){
+            return T::neutral;
+        }
+        if (s >= l && e <= r){
+            return st[i];
+        }
+
+        int mid = (s + e) / 2;
+        return query(2 * i, s, mid, l, r).op(query(2 * i + 1, mid + 1, e, l, r));
     }
-    cout << ans << "\n";
-}
+
+    T query(int l, int r){
+        return query(1, 1, n, l, r);
+    }
+
+    void update(int i, int s, int e, int l, int r, int val){
+        if (lazy[i].val != 0){
+            propagate(i, s, e, lazy[i]);
+            lazy[i].val = 0;
+        }
+
+        if (r < s || e < l){
+            return;
+        }
+        if (s >= l && e <= r){
+            propagate(i, s, e, val);
+            return;
+        }
+
+        int mid = (s + e) / 2;
+        update(2 * i, s, mid, l, r, val);
+        update(2 * i + 1, mid + 1, e, l, r, val);
+        st[i] = st[2 * i].op(st[2 * i + 1]);
+    }
+
+    void update(int l, int r, int val){
+        update(1, 1, n, l, r, val);
+    }
+};
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    t = 1;
-    //cin >> t;
-    while (t--){
-        solve();
+	
+    int w, h, n, x, y, ans = 0;
+    cin >> w >> h >> n;
+    vector <P> a(2 * n);
+    for (int i = 0; i < n; i++){
+        cin >> x >> y;
+        x += DELTA, y += DELTA;
+        a[i] = P(x, y, 1);
+        a[i + n] = P(x + w, y + h, -1);
     }
+    sort(a.begin(), a.end());
+
+    SegTree <Max> st;
+    st.init(2 * DELTA);
+    for (auto [x, y, type] : a){
+        if (type == 1){
+            st.update(y, y + h, 1);
+        }
+        else{
+            st.update(y - h, y, -1);
+        }
+        ans = max(ans, st.query(1, 2 * DELTA).val);
+    }
+    cout << ans << "\n";
+
     return 0;
 }   
